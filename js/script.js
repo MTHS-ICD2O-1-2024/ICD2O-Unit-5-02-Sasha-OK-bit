56e28d077045dc3ce00619402c8e5ea7ffd17fc5
Sasha-OK-bit <sasha.vorontsov@stu.ocsb.ca>
2025-04-22 17:19:02 +0000

    Added 5-01 Template

--- js/script.js
+++ js/script.js
@@ -1,5 +1,23 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Sasha Vorontsov
+// Created on: April 2025
 // This file contains the JS functions for index.html
+
+const randomNumber = Math.floor(Math.random() * 6) + 1
+
+function calculaterandomnumber () {
+
+  const userNumber = parseInt(document.getElementById('usernumber').value)
+
+  if (userNumber === randomNumber)  {
+    document.getElementById('answer1').innerHTML =
+      '<p>You have got it correct with your guess!</p>'
+  }
+
+  if (userNumber !== randomNumber)  {
+    document.getElementById('answer1').innerHTML =
+      '<p>You have done wrong with your guess!</p>'
+      '<br/> The correct number was: ' + randomNumber + '</p>'
+  }
+}
